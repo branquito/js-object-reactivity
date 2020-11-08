3337e090bfdae325a039ef9f16f02557c45097ec
branchito <brankompas@gmail.com>
2020-11-08 21:45:07 +0100

    Add basic reactivity system
    Separate logic for observe & notify into separate file
    
    Give an example on how we might use this, to make an object reactive
    to changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-import "./styles.css";
+import './styles.css';
+import { observe, notify } from './signals'
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+function Reactive(dataObj) {
+  observeData(dataObj);
+  
+  return {
+    data: dataObj,
+    observe,
+    notify
+  }
+  
+  function makeReactive (obj, key) {
+    let val = obj[key];
+
+    Object.defineProperty(obj, key, {
+      get() {
+        return val;
+      },
+      
+      set (newValue) {
+        val = newValue;
+        notify(key);
+      }
+    })
+  }
+
+  function observeData(obj) {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        makeReactive(obj, key);
+      }
+    }
+  }
+}
+
+let data = {
+  firstName: 'Branchito',
+  lastName: 'de Munze',
+  age: 45
+};
+
+const r = new Reactive(data);
+r.observe('firstName', () => console.log('First name has changed'));
+r.observe('firstName', () => console.log('I am also called when firstName has been changed'));
+r.observe('age', () => console.log('Age has changed'));
+
+r.data.firstName = 'paco';
+r.data.age = 44;
